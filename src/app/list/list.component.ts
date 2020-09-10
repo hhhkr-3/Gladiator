f0578ac4c40c854aed748ab4041b30434d05fdc7
hrishi <hrishi4321@gmail.com>
2020-09-10 12:42:04 +0530

    first commit

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService} from '../http.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+
+  constructor(private _http:HttpService) { }
+
+  ngOnInit(): void {
+    this._http.myMethod();
+  }
+
+}
